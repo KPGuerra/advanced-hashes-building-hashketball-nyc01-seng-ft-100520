5eec56a09676ddf082556006b8e94b3319fc5814
Karina Guerra <kpguerra96@gmail.com>
2020-09-24 00:14:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,17 +5,18 @@ def game_hash
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
       :players => [
-        :player_name,
-        :number,
-        :shoe,
-        :points,
-        :rebounds,
-        :assists,
-        :steals,
-        :blocks,
-        :slam_dumnks
+        {
+          :player_name => "Alan Anderson",
+          :number => 0,
+          :shoe => 16,
+          :points => 22,
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dumnks => 1
+        }
       ]
-    }
     :away => {
       
     }
